aebf7debf586354badee036e61e7f4d712a5206a
Daniel Chua <e1384443@u.nus.edu>
2026-01-30 11:38:29 +0800

    Change duke to jeff

---
+++ src/main/java/Jeff.java
@@ -0,0 +1,13 @@
+public class Jeff {
+    public static void main(String[] args) {
+        String logo = 
+    "(_)     / _|/ _|  \n"
+  + "  _  ___| |_| |_  \n"
+  + " | |/ _ \\  _|  _| \n"
+  + " | |  __/ | | |  \n"
+  + " | |\\___|_| |_|   \n"
+  + "_/ |             \n"
+  + "|__/         \n";
+        System.out.println("Hello from\n" + logo);
+    }
+}
